c6f1a8f023ccf9dfa85777948a12f950792d5042
王韵茜 <wangyunxi@ysccc.com>
2018-09-27 16:33:51 +0800

    添加登陆界面

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.web_view);
+        webView.loadUrl("http://imgtest.ysccc.com/appweb");
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
     }
 }
